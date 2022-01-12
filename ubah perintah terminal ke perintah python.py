46fa17e25f7f316c2f0c507800013ed09319f9ed
yuyukaliserayu <97535375+yuyukaliserayu@users.noreply.github.com>
2022-01-13 00:20:06 +0700

    Update ubah perintah terminal ke perintah python.py

--- ubah perintah terminal ke perintah python.py
+++ ubah perintah terminal ke perintah python.py
@@ -1,13 +1,11 @@
 #1. Mengubah perintah di terminal menjadi perintah di python
-#root@kali: ~ # ifconfig eth0 down	1. nonaktifkan device target
+#ini adalah perintah di terminal
+#root@kali: ~ # ifconfig eth0 down	                      1. nonaktifkan device target
 #root@kali: ~ # ifconfig eth0 hw ether 08:00:27:0e:34:8d	2. ubah mac address
-#root@kali: ~ # ifconfig eth0 up	3. aktifkan device target
+#root@kali: ~ # ifconfig eth0 up	                        3. aktifkan device target
 
 #ubah ke perintah python
-import subprocess
-subprocess.call('ifconfig ' + interface + ' down', shell=True)	#1. nonaktifkan device target
+import subprocess                                                                   #0. import fungsi
+subprocess.call('ifconfig ' + interface + ' down', shell=True)	                    #1. nonaktifkan device target
 subprocess.call('ifconfig ' + eth0 + ' hw ether '+ 08:00:27:0e:34:8d, shell=True) 	#2. ubah mac address
-subprocess.call('ifconfig ' + interface + ' up', shell=True)	#3. aktifkan device target
-
-
-
+subprocess.call('ifconfig ' + interface + ' up', shell=True)	                      #3. aktifkan device target
